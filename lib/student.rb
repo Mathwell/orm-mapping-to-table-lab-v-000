302d01303b0b19af4778dcd857e98fd319b3926a
Olena Ageyeva <eageeva@hotmail.com>
2017-12-02 15:59:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,8 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id=nil,name,grade)
-    @id, @name, @grade= id, name,grade
+  def initialize(name,grade)
+    @name, @grade= id, name,grade
   end
 
   def self.create_table
